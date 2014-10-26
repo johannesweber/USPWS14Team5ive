5fdc1e886b60562bb2557154a17f25e53d228a70
Johannes Weber <weber.johanes@gmail.com>
2014-10-26 08:18:08 +0100

    alles neu hinzugefügt

---
+++ Oauth/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  USPWS14Team5ive
+//
+//  Created by Johannes Weber on 26.10.14.
+//  Copyright (c) 2014 Johannes Weber. All rights reserved.
+//
+
+import Foundation
+
+let Withings =
+[
+    "consumerKey": "0b1de1b1e2473372f5e8e30d0f13e38f9b20c84320cf8243517e73c0c084",
+    "consumerSecret": "cdb631b4102893076d6feb038fd5fe7fd28431b998881d5c001307cece802"
+]
+
+let Fitbit =
+[
+    "consumerKey": "7c39abf127964bc984aba4020845ff11",
+    "consumerSecret": "18c4a92f21f1458e8ac9798567d3d38c"
+]
--- USPWS14Team5ive/ViewController.swift
+++ USPWS14Team5ive/ViewController.swift
@@ -8,18 +8,110 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var services = ["Fitbit", "Withings"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = "OAuth"
+        let tableView: UITableView = UITableView(frame: self.view.bounds, style: .Plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView);
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func doOAuthFitbit(){
+        let oauthswift = OAuth1Swift(
+            consumerKey:    Fitbit["consumerKey"]!,
+            consumerSecret: Fitbit["consumerSecret"]!,
+            requestTokenUrl: "https://api.fitbit.com/oauth/request_token",
+            authorizeUrl:    "https://www.fitbit.com/oauth/authorize",
+            accessTokenUrl:  "https://api.fitbit.com/oauth/access_token"
+        )
+        oauthswift.authorizeWithCallbackURL( NSURL(string: "oauth-callback://oauth-callback/fitbit")!, success: {
+            credential, response in
+            self.showAlertView("Fitbit", message: "auth_token:\(credential.oauth_token)\n\noauth_token_secret:\(credential.oauth_token_secret)")
+            var parameters =  Dictionary<String, AnyObject>()
+            oauthswift.client.get("https://api.fitbit.com/1/user/-/profile.json", parameters: parameters,
+                success: {
+                    data, response in
+                    let jsonDict: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil)
+                    println(jsonDict)
+                }, failure: {(error:NSError!) -> Void in
+                    println(error)
+            })
+            
+            var parameters2 =  Dictionary<String, AnyObject>()
+            parameters2 = [ "amount" : "1000.0",
+                "date" : "2014-10-23"
+            ]
+            oauthswift.client.post("https://api.fitbit.com/1/user/-/foods/log/water.json", parameters: parameters2,
+                success: {
+                    data, response in
+                    let jsonDict: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil)
+                    println(jsonDict)
+                }, failure: {(error:NSError!) -> Void in
+                    println(error)
+            })
+            
+            }, failure: {(error:NSError!) -> Void in
+                println(error.localizedDescription)
+        })
+    }
+    
+    func doOAuthWithings(){
+        let oauthswift = OAuth1Swift(
+            consumerKey:    Withings["consumerKey"]!,
+            consumerSecret: Withings["consumerSecret"]!,
+            requestTokenUrl: "https://oauth.withings.com/account/request_token",
+            authorizeUrl:    "https://oauth.withings.com/account/authorize",
+            accessTokenUrl:  "https://oauth.withings.com/account/access_token"
+        )
+        oauthswift.authorizeWithCallbackURL( NSURL(string: "oauth-callback://oauth-callback/withings")!, success: {
+            credential, response in
+            self.showAlertView("Withings", message: "oauth_token:\(credential.oauth_token)\n\noauth_token_secret:\(credential.oauth_token_secret)")
+            }, failure: {(error:NSError!) -> Void in
+                println(error.localizedDescription)
+        })
+    }
+    
+    
+    func showAlertView(title: String, message: String) {
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
+        return services.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel.text = services[indexPath.row]
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        var service: String = services[indexPath.row]
+        switch service {
+        case "Fitbit":
+            doOAuthFitbit()
+        case "Withings":
+            doOAuthWithings()
+        default:
+            println("default")
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated:true)
+    }
+    
+    
 }
+
+
 

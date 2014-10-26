64119ba4fd7860acdfe3c337c6db1903a8cd6f2e
Johannes Weber <weber.johanes@gmail.com>
2014-10-26 07:54:39 +0100

    Initial Commit

---
+++ USPWS14Team5ive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USPWS14Team5ive
+//
+//  Created by Johannes Weber on 26.10.14.
+//  Copyright (c) 2014 Johannes Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

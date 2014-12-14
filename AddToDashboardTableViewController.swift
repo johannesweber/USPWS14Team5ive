2ab1f63eb2a302457bd5a805fd6f308228c73eb1
Johannes Weber <weber.johanes@gmail.com>
2014-12-14 12:40:11 +0100

    added table view to addToDashboard Screen

---
+++ AddToDashboardTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddToDashboardViewController.swift
+//  USPWS14Team5ive
+//
+//  Created by Johannes Weber on 30.11.14.
+//  Copyright (c) 2014 Johannes Weber. All rights reserved.
+//
+
+import UIKit
+
+class AddToDashboardTableViewController: UIViewTableController {
+    
+}
